6f933f9cda8b5da5a16369d2c5d0dd4c3afcef92
gawandegk <54916653+gawandegk@users.noreply.github.com>
2020-06-05 11:33:02 -0600

    adding new files to master

---
+++ Pnum.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+
+public class Pnum {
+	@SuppressWarnings("unused")
+	public static void main (final String args[]) {
+        int n;
+        int status = 1;
+        int num = 3;
+
+        final Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("enter value of n");
+			n = scanner.nextInt();
+			
+			if (n >= 1)
+			{
+				System.out.println("First "+n+" prime numbers are");
+				
+				System.out.println(2);
+			}
+			
+			
+			for (int i = 2; i <= n;)
+			{
+				for  (int j = 2; j <= Math.sqrt(num); j++ )
+						{
+							if (num%j == 0);
+							{
+								status = 0;
+								break;
+							}
+						}
+		
+				if (status !=0)
+				{
+				System.out.println(num);
+				i++;
+				}
+				status = 1;
+				num++;
+			}
+	}
+}
